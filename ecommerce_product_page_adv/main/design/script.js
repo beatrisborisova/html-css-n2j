44145bcf2c4bd49ad702a0c140b8c27d6161552f
Beatris <beti16b@gmail.com>
2022-07-06 12:53:39 +0300

    ecommerce cart

--- ecommerce_product_page_adv/main/design/script.js
+++ ecommerce_product_page_adv/main/design/script.js
@@ -1,10 +1,19 @@
+let quantity = document.getElementById('quantity');
+let isCartShown = false;
+let isMobileNavOpened = false;
+let counter = 0;
+
 document.body.addEventListener('click', (e) => {
-    if (e.target.tagName == 'IMG') {
+    if (e.target.tagName == 'IMG' && e.target.parentElement.classList.contains('image-wrapper-avatar')) {
         e.target.classList.add('active-avatar')
-    } else {
+    }
+    if (!e.target.classList.contains('icon-cart')) {
+        isCartShown = true;
+        showCart()
+    }
+    else {
         document.querySelector('.avatar-image').classList.remove('active-avatar');
-        document.querySelector('.my-cart').style.display = 'none';
-        isCartShown = false;
+
     }
 })
 
@@ -15,7 +24,8 @@ document.querySelector('.icon-cart').addEventListener('click', showCart);
 document.querySelector('.bin').addEventListener('click', removeItemFromCart);
 document.querySelector('.main-image').addEventListener('click', toggleImage);
 document.querySelector('.tumbnails-wrapper').addEventListener('click', showImage);
-document.querySelector('.navs-container').addEventListener('click', toggleMobileNav);
+document.querySelector('.menu').addEventListener('click', showMobileNav);
+document.querySelector('.lightbox-product-images-svg-container').addEventListener('click', hideMobileNav);
 
 const count = document.getElementById('count');
 const total = document.getElementById('total');
@@ -25,10 +35,7 @@ const productImagesContainer = document.querySelector('.lightbox-product-images-
 const mainImageLight = document.querySelector('.main-image-light');
 const pseudoNumberContainer = document.querySelector('.icon-cart');
 
-let quantity = document.getElementById('quantity');
-let isCartShown = false;
-let isMobileNavOpened = false;
-let counter = 0;
+
 
 function addRemove(e) {
     if (e.target.id == 'minus' || e.target.parentElement.id == 'minus') {
@@ -56,7 +63,6 @@ function addToCart() {
 }
 
 function showCart() {
-
     if (isCartShown) {
         document.querySelector('.my-cart').style.display = 'none';
         isCartShown = false;
@@ -69,26 +75,34 @@ function showCart() {
 function removeItemFromCart() {
     cartContentEmpty.style.display = 'block';
     cartContentFilled.style.display = 'none';
+    pseudoNumberContainer.title = 0;
 }
 
 function toggleImage() {
     productImagesContainer.style.display = 'block';
     let imageIndex = 1;
     productImagesContainer.addEventListener('click', (e) => {
+
         if (e.target.tagName == 'IMG') {
             imageIndex = e.target.classList[0];
             Array.from(e.target.parentElement.parentElement.children).forEach(el => {
                 el.classList.remove('active-image')
             })
             e.target.parentElement.classList.add('active-image')
-
             if (!Number.isNaN(imageIndex)) {
                 console.log(mainImageLight);
                 mainImageLight.innerHTML = `<img src="../../images/image-product-${imageIndex}.jpg" alt="image-product-${imageIndex}" class="${imageIndex}">`
             }
+        } else if (e.target.classList.contains('image-wrapper-after') || e.target.parentElement.classList.contains('image-wrapper-after')) {
+            let parent;
 
-        } else if (e.target.classList.contains('image-wrapper-after') || e.target.parentElement.classList.contains('.image-wrapper-after')) {
-            imageIndex = Number(e.target.parentElement.children[1].children[0].className) + 1
+            if (e.target.classList.contains('image-wrapper-after')) {
+                parent = e.target
+                imageIndex = Number(e.target.parentElement.children[1].children[0].className) + 1
+            } else {
+                parent = e.target.parentElement.parentElement
+                imageIndex = Number(parent.children[1].children[0].className) + 1
+            }
 
             if (imageIndex == 0) {
                 imageIndex = 4
@@ -96,10 +110,17 @@ function toggleImage() {
             if (imageIndex == 5) {
                 imageIndex = 1
             }
-
             mainImageLight.innerHTML = `<img src="../../images/image-product-${imageIndex}.jpg" alt="image-product-${imageIndex}" class="${imageIndex}">`
-        } else if (e.target.classList.contains('image-wrapper-before') || e.target.parentElement.classList.contains('.image-wrapper-before')) {
-            imageIndex = Number(e.target.parentElement.children[1].children[0].className) - 1
+        } else if (e.target.classList.contains('image-wrapper-before') || e.target.parentElement.classList.contains('image-wrapper-before')) {
+            let parent;
+
+            if (e.target.classList.contains('image-wrapper-before')) {
+                parent = e.target
+                imageIndex = Number(e.target.parentElement.children[1].children[0].className) - 1
+            } else {
+                parent = e.target.parentElement.parentElement
+                imageIndex = Number(parent.children[1].children[0].className) - 1
+            }
             if (imageIndex == 0) {
                 imageIndex = 4
             }
@@ -130,11 +151,15 @@ function showImage(e) {
 
 }
 
-function toggleMobileNav() {
+function showMobileNav() {
     if (!isMobileNavOpened) {
         document.querySelector('.navs-container-ul').style.display = 'block';
         isMobileNavOpened = true;
-    } else {
+    }
+}
+
+function hideMobileNav() {
+    if (isMobileNavOpened) {
         document.querySelector('.navs-container-ul').style.display = 'none';
         isMobileNavOpened = false;
     }
